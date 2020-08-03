85866ba4e7d4830ab20a23abd5f1f0d1daee6c09
levannghia <levannghia.tdc2018@gmail.com>
2020-08-03 22:47:33 +0700

    Hoan thanh nhiem vu

---
+++ app/src/main/java/com/tdc/edu/vn/myapplication/ActivityDangKy.java
@@ -0,0 +1,74 @@
+package com.tdc.edu.vn.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ActivityDangKy extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    EditText edtUser;
+    EditText edtPassword;
+    Button btnDangKy;
+    Button btnDangNhap;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dangky);
+        edtUser = (EditText) findViewById(R.id.username);
+        edtPassword = (EditText) findViewById(R.id.password);
+        btnDangKy = (Button) findViewById(R.id.DangKy);
+        btnDangNhap = (Button) findViewById(R.id.login);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnDangKy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DangKy();
+            }
+        });
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(ActivityDangKy.this,ActivityDangNhap.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void DangKy(){
+        final String email = edtUser.getText().toString();
+        final String password = edtPassword.getText().toString();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(ActivityDangKy.this, "Đăng Ký Thành Công!",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(ActivityDangKy.this, "Đăng Ký Thất Bại",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
